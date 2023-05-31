39b274d7671839a31a6759e48100ba4e1c78c601
Dmitry Vasilevich <wotlk777@gmail.com>
2023-06-01 00:58:52 +0200

    display chrome tabs in column

--- src/youtube-tracking.tsx
+++ src/youtube-tracking.tsx
@@ -1,11 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function YoutubeTracking() {
-    const [url, seUrl] = useState("");
+    const MISSING_PERMISSION_TAB = "Extension is missing \"tab\" permission";
+    const UNDEFINED_TAB = "UNDEFINED TAB";
+
+    const [urls, setUrls] = useState<string[]>([]);
+
+    useEffect(() => {
+        const getTabsByQuery = async (query: chrome.tabs.QueryInfo) => {
+            if (!chrome.tabs) {
+                console.error(MISSING_PERMISSION_TAB);
+                return;
+            }
+
+            let tabs = await chrome.tabs.query(query);
+
+            console.log(tabs);
+
+            setUrls(tabs.map(tab => tab.url ?? UNDEFINED_TAB));
+        }
+
+        let query : chrome.tabs.QueryInfo = {
+            currentWindow: true
+        };
+
+        getTabsByQuery(query);
+    }, []);
+
+    const toUrlComponent = (url: string) => {
+        return(
+            <li>{url}</li>
+        );
+    }
 
     return(
         <div className="youtube-tracking">
-            <p>hehe I'm the YoutubeTracking component</p>
+            <h2>YOUTUBE TRACKING AREA</h2>
+            <ul>{urls.map(toUrlComponent)}</ul>
         </div>
     );
 }
