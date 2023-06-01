10b1143c8b11e3ee2c7ae85b2886a7bda3fa008a
Dmitry Vasilevich <wotlk777@gmail.com>
2023-06-01 20:46:09 +0200

    display tracking tab data for tabs

--- src/youtube-tracking.tsx
+++ src/youtube-tracking.tsx
@@ -1,42 +1,44 @@
 import { useEffect, useState } from "react";
+import { TrackingTabData } from "./types/tracking-tab-data";
 
 export function YoutubeTracking() {
-    const MISSING_PERMISSION_TAB = "Extension is missing \"tab\" permission";
     const UNDEFINED_TAB = "UNDEFINED TAB";
 
-    const [urls, setUrls] = useState<string[]>([]);
+    const [tabs, setTabs] = useState<TrackingTabData[]>([]);
 
     useEffect(() => {
         const getTabsByQuery = async (query: chrome.tabs.QueryInfo) => {
-            if (!chrome.tabs) {
-                console.error(MISSING_PERMISSION_TAB);
-                return;
-            }
-
             let tabs = await chrome.tabs.query(query);
 
             console.log(tabs);
 
-            setUrls(tabs.map(tab => tab.url ?? UNDEFINED_TAB));
+            setTabs(tabs.map(tab => ({
+                favIconUrl: tab.favIconUrl,
+                url: tab.url,
+                title: tab.title ?? UNDEFINED_TAB
+            }) as TrackingTabData));
         }
 
-        let query : chrome.tabs.QueryInfo = {
+        let query: chrome.tabs.QueryInfo = {
             currentWindow: true
         };
 
         getTabsByQuery(query);
     }, []);
 
-    const toUrlComponent = (url: string) => {
-        return(
-            <li>{url}</li>
+    const toTabComponent = (tab: TrackingTabData) => {
+        return (
+            <li>
+                <img src={tab.favIconUrl} style={{height: 16, width: 16}}></img>
+                {tab.title} | {tab.url}
+            </li>
         );
     }
 
-    return(
+    return (
         <div className="youtube-tracking">
             <h2>YOUTUBE TRACKING AREA</h2>
-            <ul>{urls.map(toUrlComponent)}</ul>
+            <ul>{tabs.map(toTabComponent)}</ul>
         </div>
     );
 }
