dd1c6bdac7e1432a15bfa35e299da466aa2c1603
Dmitry Vasilevich <wotlk777@gmail.com>
2023-06-15 01:01:44 +0200

    dark theme usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import './App.css';
 import { DummyTracking } from './tabs-tracking/context/dummy-tracking/dummy-tracking';
 import { YoutubeTracking } from './tabs-tracking/context/youtube-tracking/youtube-tracking';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   const MISSING_PERMISSION_TAB = "Extension is missing \"tab\" permission";
@@ -14,13 +21,15 @@ function App() {
   }
 
   return (
-    <div className="App">
-      {missingPermissons ? (
-        <DummyTracking />
-      ) : (
-        <YoutubeTracking />
-      )}
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        {missingPermissons ? (
+          <DummyTracking />
+        ) : (
+          <YoutubeTracking />
+        )}
+      </div>
+    </ThemeProvider>
   );
 }
 
