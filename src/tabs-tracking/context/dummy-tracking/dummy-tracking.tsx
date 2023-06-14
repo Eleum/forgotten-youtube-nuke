b7eb79cfbca7ccd0feb7cd7a429ce9ab513975b3
Dmitry Vasilevich <wotlk777@gmail.com>
2023-06-14 02:00:59 +0200

    random dates for tabs, assign tabs to periods

--- src/tabs-tracking/context/dummy-tracking/dummy-tracking.tsx
+++ src/tabs-tracking/context/dummy-tracking/dummy-tracking.tsx
@@ -1,6 +1,8 @@
 import { YouTube } from "@mui/icons-material";
-import { Button, Tooltip } from "@mui/material";
-import { TrackingTab } from "../../types/tracking-tab";
+import { Button, Tooltip, Typography } from "@mui/material";
+import { TrackingTab, TrackingTabWithDate } from "../../types/tracking-tab";
+import { TrackingPeriod } from "../../types/tracking-period";
+import { addDays, addMonths, addWeeks, minTime, setDay, startOfMonth, startOfToday, startOfWeek } from "date-fns";
 
 export function DummyTracking() {
     const dummyData: TrackingTab[] = [
@@ -27,21 +29,91 @@ export function DummyTracking() {
         }
     ];
 
-    const toUiComponent = (tab: TrackingTab) => {
+    const today = new Date();
+    const thisWeek = addDays(today, -3);
+    const lastWeek = addWeeks(today, -1);
+    const twoWeeksAndMore1 = setDay(addWeeks(today, -2), 1);
+    const twoWeeksAndMore2 = setDay(addWeeks(today, -2), 3);
+    const month = addDays(addWeeks(addMonths(today, -1), -1), -2);
+    const other = addDays(addWeeks(addMonths(today, -2), -2), -4);
+
+    const dates = [today, thisWeek, lastWeek, twoWeeksAndMore1, twoWeeksAndMore2, month, other];
+
+    let data = Array
+        .from(Array(10), (x, i) => i + 1)
+        .map(() => ({
+            id: crypto.randomUUID(),
+            date: dates[~~(Math.random() * (6 - 0 + 1) + 0)],
+            ...dummyData[~~(Math.random() * (2 - 0 + 1) + 0)]
+        }) as TrackingTabWithDate);
+
+    const fromToday = startOfToday();
+    const fromThisWeek = startOfWeek(fromToday);
+    const fromLastWeek = startOfWeek(addWeeks(fromToday, -1));
+    const fromTwoWeeks = startOfWeek(addWeeks(fromToday, -2));
+    const fromThisMonth = startOfMonth(addMonths(fromToday, -1));
+
+    const periods = [
+        fromToday,
+        fromThisWeek,
+        fromLastWeek,
+        fromTwoWeeks,
+        fromThisMonth,
+        undefined
+    ].map(selector => ({
+        from: selector,
+        items: []
+    }) as TrackingPeriod);
+
+    AssignTabsToPeriods(data, periods);
+
+    const toUiComponent = (period: TrackingPeriod) => {
+        const UNDEFINED_PERIOD_KEY = "UNDEFINED-PERIOD-KEY";
+        const periodFrom = period.from?.toString();
+
+        const toTabUiComponent = (tab: TrackingTab) => {
+            return (
+                <Tooltip key={tab.id} title={tab.url}>
+                    {tab.discarded ? (
+                        <Button variant="outlined" startIcon={<YouTube />} color="error">{tab.title}</Button>
+                    ) : (
+                        <Button variant="contained" startIcon={<YouTube />} color="error" disableElevation>{tab.title}</Button>
+                    )}
+                </Tooltip>
+            );
+        }
+        
         return (
-            <Tooltip title={tab.url}>
-                {tab.discarded ? (
-                    <Button variant="outlined" startIcon={<YouTube /> } color="error">{tab.title}</Button>
-                ) : (
-                    <Button variant="contained" startIcon={<YouTube />} color="error" disableElevation>{tab.title}</Button>
-                )}
-            </Tooltip>
+            <div key={periodFrom ?? UNDEFINED_PERIOD_KEY}>
+                <Typography variant="caption" display="block">
+                    {period.from == undefined ? "others" : `from ${periodFrom}`}
+                </Typography>
+                {period.items.map(toTabUiComponent)}
+            </div>
         );
-    }
+    };
 
     return (
         <div className="dummy-tracking">
-            {dummyData.map(toUiComponent)}
+            {periods.map(toUiComponent)}
         </div>
-    )
+    );
+}
+
+function AssignTabsToPeriods(tabs: TrackingTabWithDate[], periods: TrackingPeriod[]) {
+    tabs.forEach(tab => {
+        let assigned = false;
+
+        for (let i = 0; i < periods.length; i++) {
+            if (tab.date >= (periods[i].from ?? minTime)) {
+                periods[i].items.push(tab);
+                assigned = true;
+                break;
+            }
+        }
+
+        if (assigned) return;
+
+        console.error(`Unable to assign the tab ${tab.id} to a period`);
+    });
 }
