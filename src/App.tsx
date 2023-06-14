f763ada18b2322a36a3037427ed1bc9880370955
Dmitry Vasilevich <wotlk777@gmail.com>
2023-06-15 01:53:04 +0200

    extract dummy tracking into separate branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import './App.css';
-import { DummyTracking } from './tabs-tracking/context/dummy-tracking/dummy-tracking';
+import { Typography } from '@mui/material';
 import { YoutubeTracking } from './tabs-tracking/context/youtube-tracking/youtube-tracking';
+import './App.css';
 
 function App() {
-  const MISSING_PERMISSION_TAB = "Extension is missing \"tab\" permission";
-  let missingPermissons = false;
+    const MISSING_PERMISSION_TAB = "Extension is missing \"tab\" permission";
+    let missingPermissons = false;
 
-  console.log(process.env.NODE_ENV);
+    console.log(process.env.NODE_ENV);
 
-  if (!chrome.tabs) {
-    missingPermissons = true;
-    console.error(MISSING_PERMISSION_TAB);
-  }
+    if (!chrome.tabs) {
+        missingPermissons = true;
+        console.error(MISSING_PERMISSION_TAB);
+    }
 
-  return (
-    <div className="App">
-      {missingPermissons ? (
-        <DummyTracking />
-      ) : (
-        <YoutubeTracking />
-      )}
-    </div>
-  );
+    return (
+        <div className="App">
+            {missingPermissons ? (
+                <Typography variant="h4">
+                    {"hello there :)"}
+                </Typography>
+            ) : (
+                <YoutubeTracking />
+            )}
+        </div>
+    );
 }
 
 export default App;
